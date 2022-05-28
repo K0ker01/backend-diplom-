6131435f6c8e124fd1e6dff6c5a9116e950d09e2
K0ker01 <99072095+K0ker01@users.noreply.github.com>
2022-05-28 14:34:46 +0300

    delete limit and page

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -30,25 +30,7 @@ class DeviceController {
     }
 
     async getAll(req, res){
-        let {brandId, typeId, limit, page} = req.query
-        page = page || 1
-        limit = limit || 9
-        let offset = page * limit - limit
-        let devices;
-        if (!brandId && !typeId){
-            devices = await Device.findAndCountAll({limit, offset})
-
-        }
-        if (brandId && !typeId){
-            devices = await Device.findAndCountAll({where: {brandId}, limit, offset})
-
-        }
-        if (!brandId && typeId){
-            devices = await Device.findAndCountAll({where: {typeId}, limit, offset})
-        }
-        if (brandId && typeId){
-            devices = await Device.findAndCountAll({where:{typeId, brandId}, limit, offset})
-        }
+        const devices = await Device.findAll()
         return res.json(devices)
 
     }
