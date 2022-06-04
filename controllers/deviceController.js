a4f6c5efdd8ecc9689d39f63be49bc183ed065b9
K0ker01 <99072095+K0ker01@users.noreply.github.com>
2022-06-05 00:40:00 +0300

    rev 2

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -6,11 +6,9 @@ const ApiError = require ('../error/ApiError')
 class DeviceController {
     async create(req, res, next){
         try{
-            let {name, price, oldprice, brandId, typeId, info} = req.body
-            const {img} = req.files
-            let fileName = uuid.v4() + ".jpg"
-            img.mv(path.resolve(__dirname, '..', 'static', fileName))
-            const device = await Device.create({name, price, oldprice, brandId, typeId, img: fileName});
+            let {name, price, oldprice, brandId, typeId, info, img} = req.body
+            
+            const device = await Device.create({name, price, oldprice, brandId, typeId, img});
            
 
             if(info){
@@ -25,7 +23,7 @@ class DeviceController {
             }
              return res.json(device)
             } catch (e){
-                next(ApiError.badRequest(e.message))}
+                next(e)}
   
     }
 
--- models/models.js
+++ models/models.js
@@ -27,7 +27,7 @@ const Device = sequelize.define('device', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
     price: {type: DataTypes.INTEGER, allowNull: false},
-    oldprice: {type: DataTypes.INTEGER, allowNull: false},
+    oldprice: {type: DataTypes.INTEGER, defaultValue: null},
     rating: {type: DataTypes.INTEGER, defaultValue: 0},
     img: {type: DataTypes.STRING, allowNull: false},
     
