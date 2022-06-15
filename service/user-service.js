f088deeba8ae6b8e110e0dae728d8cb6f0b13755
K0ker01 <99072095+K0ker01@users.noreply.github.com>
2022-06-15 03:31:49 +0300

    role/2

--- service/user-service.js
+++ service/user-service.js
@@ -25,7 +25,7 @@ class UserService {
         const tokens = tokenService.generateTokens({...userDto});
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
-        return {...tokens, user: userDto} 
+        return {user: userDto, ...tokens} 
     }
 
     async activate(activationLink) {
